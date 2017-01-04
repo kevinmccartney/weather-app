4ee46ed4429d082979250b8b058afabb05841ca1
Kevin McCartney <hello@kevinmccartney.is>
2017-01-04 13:16:38 -0500

    Added a config & set up the index to make the network request

--- WeatherApp/index.ios.js
+++ WeatherApp/index.ios.js
@@ -9,16 +9,36 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  AlertIOS
 } from 'react-native';
+import { apiKey } from "./config"
 
 export default class WeatherApp extends Component {
+
+  sendReq = () => {
+    fetch("https://api.apixu.com/v1/current.json?key=" + apiKey + "&q=43081")
+    .then((response) => response.json())
+    .then((response) => {
+      AlertIOS.alert(
+        "Response",
+        JSON.stringify(response)
+      )
+    })
+    .done()
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.instructions}>
           MyWeatherApp
         </Text>
+        <Button
+          title="Send Request"
+          onPress={this.sendReq}
+        />
       </View>
     );
   }
