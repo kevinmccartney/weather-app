1a44eac10bb0da8c1c5cd04316ae34711804143a
Kevin McCartney <hello@kevinmccartney.is>
2017-01-04 17:22:24 -0500

    The promise implementation is way better now :thumbsup:

--- WeatherApp/index.ios.js
+++ WeatherApp/index.ios.js
@@ -17,42 +17,50 @@ import { WEATHER_KEY, GOOGLE_KEY } from "./config"
 
 export default class WeatherApp extends Component {
 
-  sendReq = () => {
-    fetch("https://api.apixu.com/v1/current.json?key=" + WEATHER_KEY + "&q=" + this.state.zipcode)
-    .then((response) => response.json())
-    .then((response) => {
-      AlertIOS.alert(
-        "Response",
-        JSON.stringify(response)
-      )
-    })
-  }
-
-  findZip = () => {
+  getCoords = () => {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           var { latitude, longitude } = position.coords
           var latLong = latitude + "," + longitude
-          resolve(
-            fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLong + "&result_type=postal_code&key=" + GOOGLE_KEY)
-            .then((response) => response.json())
-            .then((response) => {
-              var zipCode = response.results[0].address_components[0].long_name
-              this.setState({ zipcode: zipCode })
-            })
-          )
-        }
-      ),
-      (error) => {
-        reject(alert(JSON.stringify(error)))
-      },
-      { enableHighAccuracy: true }
+          this.setState({lat_long: latLong})
+          resolve(latLong)
+        },
+        (error) => {
+          reject(alert(JSON.stringify(error)))
+        },
+        { enableHighAccuracy: true }
+      )
+    })
+  }
+
+  getZip = (latLong) => {
+    return new Promise((resolve, reject) => {
+      fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLong + "&result_type=postal_code&key=" + GOOGLE_KEY)
+      .then((response) => response.json())
+      .then((response) => {
+        var zipCode = response.results[0].address_components[0].long_name
+        this.setState({ zipcode: zipCode })
+        resolve(zipCode)
+      })
+    })
+  }
+
+  getWeather = (zip) => {
+    fetch("https://api.apixu.com/v1/current.json?key=" + WEATHER_KEY + "&q=" + zip)
+    .then((response) => response.json())
+    .then((response) => {
+      AlertIOS.alert(
+        "Response URL",
+        JSON.stringify(response)
+      )
     })
   }
 
   componentDidMount() {
-    this.findZip()
+    this.getCoords()
+    .then(latLong => this.getZip(latLong))
+    .then(zip => this.getWeather(zip))
   }
 
   render() {
@@ -61,10 +69,6 @@ export default class WeatherApp extends Component {
         <Text style={styles.instructions}>
           MyWeatherApp
         </Text>
-        <Button
-          title="Send Request"
-          onPress={this.sendReq}
-        />
       </View>
     );
   }
